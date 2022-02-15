7248abba8f2f5437aab7f80509135992f2de4dea
MovsisyanN <movsisyan@protonmail.com>
2022-02-15 10:55:33 +0400

    db ddl lecture

--- DBandDistributedSystems/local_playground.sql
+++ DBandDistributedSystems/local_playground.sql
@@ -1,13 +1,21 @@
--- create a table named test_table that has an incrementing id
-CREATE TABLE test_table_3 (
-    id serial PRIMARY KEY,
-    name VARCHAR(20)
+drop table youtube_user, youtube_video;
+
+create table youtube_user (
+    email varchar(50) primary key,
+    fullname varchar(50) default 'empty',
+    registeredOn date default CURRENT_DATE
 );
 
--- Language: sql
--- insert a row into test_table
-insert into test_table (name) values ('two');
+insert into youtube_user (email, fullname) 
+values ('mher2@movsisyan.info', 'Mher2 Movsisyan');
 
+create table youtube_video (
+    url varchar(30),
+    title varchar(30),
+    uploader_id VARCHAR(50) REFERENCES youtube_user(email),
+    numbers_watched int 
+);
 
--- Language: sql
-select * from test_table
+insert into youtube_video values 
+('tttt', 'rick roll', 'mher2@movsisyan.info');
+
