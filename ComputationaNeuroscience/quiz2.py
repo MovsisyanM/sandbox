ad9e3469dc86c3f4efc194b1e707eb3505a5449f
Movsisyan <movsisyan@protonmail.com>
2023-06-19 21:14:52 +0400

    Neuroscience week 2 sync

---
+++ ComputationaNeuroscience/quiz2.py
@@ -0,0 +1,32 @@
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pickle
+
+from compute_sta import compute_sta
+
+
+FILENAME = 'c1p8.pickle'
+
+with open(FILENAME, 'rb') as f:
+    data = pickle.load(f)
+    
+stim = data['stim']
+rho = data['rho']
+    
+
+# Fill in these values
+sampling_period = 2 # in ms
+num_timesteps = 150
+
+sta = compute_sta(stim, rho, num_timesteps)
+
+time = (np.arange(-num_timesteps, 0) + 1) * sampling_period
+
+plt.plot(time, sta)
+plt.xlabel('Time (ms)')
+plt.ylabel('Stimulus')
+plt.title('Spike-Triggered Average')
+
+plt.show()
